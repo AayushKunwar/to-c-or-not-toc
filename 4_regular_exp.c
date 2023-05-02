61f82c03252a37f4043e76bb767a57d5c0568ab4
Aayush Kunwar <kunwar_aayush@live.com>
2023-05-02 10:01:39 +0545

    renamed stuff

--- 4_regular_exp.c
+++ 4_regular_exp.c
@@ -4,28 +4,29 @@
 #include<stdio.h>
 #include<string.h>
 
-int main(){
+int main() {
     char input[] = "abba";
-    printf("for input: %s\n",input);
+    printf("for input: %s\n", input);
     char state = 'P';
-    for (int i = 0; i < strlen(input); i++)
-    {
-       if(state =='P' && input[i] == 'a'){
-        state = 'Q';
-       } 
-       else if(state == 'P' && input[i] == 'b'){
-        printf("not accepted");
-        return 0;
-       } else if(state == 'Q' && input[i] == 'b'){
-        state = 'Q';
-       } else if(state == 'Q' && input[i] == 'a'){
-        state = 'P';
-       }
+    for (int i = 0; i < strlen(input); i++) {
+        if (state == 'P' && input[i] == 'a') {
+            state = 'Q';
+        }
+        else if (state == 'P' && input[i] == 'b') {
+            printf("not accepted");
+            return 0;
+        }
+        else if (state == 'Q' && input[i] == 'b') {
+            state = 'Q';
+        }
+        else if (state == 'Q' && input[i] == 'a') {
+            state = 'P';
+        }
     }
-    if(state == 'P'){
+    if (state == 'P') {
         printf("accepted\n");
     }
-    else{
+    else {
         printf("not accepted\n");
     }
 }
