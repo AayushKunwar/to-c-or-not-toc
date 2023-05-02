9e28d8939ec4d38c7344ae1fad7222b3338b7aa2
Aayush Kunwar <kunwar_aayush@live.com>
2023-05-02 21:25:55 +0545

    fermat's last theorum

--- fermats_last_theorum.c
+++ fermats_last_theorum.c
@@ -5,16 +5,16 @@
 #include<stdio.h>
 #include<math.h>
 
-int main(){
-    int n,total,x,y,z;
+int main() {
+    int n, total, x, y, z;
     printf("Enter the power n:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     total = 3;
-    while(1){
-        for (x=1; x<=total-2; x++){
-            for(y=1; y<=total-x-1; y++){
-                z = total -x-y;
-                if(pow(x,n) + pow(y,n) == pow(z,n)){
+    while (1) {
+        for (x = 1; x <= total - 2; x++) {
+            for (y = 1; y <= total - x - 1; y++) {
+                z = total - x - y;
+                if (pow(x, n) + pow(y, n) == pow(z, n)) {
                     printf("hello world\n");
                 }
             }
